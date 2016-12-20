5ceb40cd93158c124822e0f26da06b3f347ea3f2
Grigory <krasilnikov@phystech.edu>
2016-12-21 02:45:34 +0300

    Homework 2

---
+++ homework1-master/src/main/java/ru/sbt/bit/ood/hw1/TradeJobMain.java
@@ -0,0 +1,25 @@
+package ru.sbt.bit.ood.hw1;
+
+/**
+ * Created by Григорий on 20.12.2016.
+ */
+
+public class TradeJobMain {
+    private final TradeUpdater updater;
+
+    private final Downloader downloader;
+    private final Parser parser;
+
+    public TradeJobMain(TradeUpdater updater, Parser parser, Downloader downloader) {
+        this.downloader = downloader;
+        this.parser = parser;
+        this.updater = updater;
+    }
+
+    public void run() {
+        String filename = downloader.downloadTradesFile();
+        Iterable<Trade> tradeRecords = parser.parse();
+        updater.updateTrades(tradeRecords);
+    }
+
+}
